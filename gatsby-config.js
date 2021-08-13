2ea06369f64935fa54847c9a8a0c3d755d3b1e24
Sanjay Yepuri <sanjay@Sanjays-MacBook-Pro.local>
2021-08-12 23:47:29 -0500

    setup i18n and gatsby bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,12 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     {
@@ -39,5 +39,41 @@ module.exports = {
       },
       __key: "pages",
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "locale",
+        path: "./src/locales"
+      }
+    },
+    {
+      resolve: "gatsby-plugin-react-i18next",
+      options: {
+        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        languages: [`en`, `es`, `de`],
+        defaultLanguage: `en`,
+        // if you are using Helmet, you must include siteUrl, and make sure you add http:https
+        // siteUrl: `https://example.com/`,
+        // you can pass any i18next options
+        // pass following options to allow message content as a key
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false // not needed for react as it escapes by default
+          },
+        },
+        pages: [
+          {
+            matchPath: '/:lang?/blog/:uid',
+            getLanguageFromPath: true,
+            excludeLanguages: ['es']
+          },
+          {
+            matchPath: '/preview',
+            languages: ['en']
+          }
+        ]
+      }
+
+    }
   ],
 };
---
+++ src/components/Footer/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Section from '../../components/Section';
+import styled from "styled-components";
+import Grid from '@material-ui/core/Grid';
+import { FONTS, COLORS } from "../../constants";
+
+const Wrapper = styled.div`
+  p{
+    font-family: ${FONTS.font2};
+    font-weight: 300;
+    color: ${COLORS.white};
+    text-align: center;
+  }
+  h2 {
+    font-family: ${FONTS.font1};
+    font-weight: 500;
+    color: ${COLORS.white};
+  }
+  a {
+    color: ${COLORS.white}
+  }
+`;
+
+const Footer = () => {
+  return (
+    <Wrapper>
+        <Section backgroundColor='darkblue' p="50px">
+          <p>
+          <Grid container direction="row" justify="center" spacing={2}>
+          <Grid item sm={4} xs={10}> <a target="_blank" rel="noopener noreferrer"  href="https://coved.org/terms">Terms and Conditions</a></Grid>
+          <Grid item sm={4} xs={10}> <a target="_blank" rel="noopener noreferrer"  href="https://coved.org/privacy">Privacy Policy</a></Grid>
+          {/* <Grid item sm={4} xs={10}> <Link to="/faqs">FAQs</Link></Grid> */}
+          </Grid>
+          </p>
+        </Section>
+    </Wrapper>
+  )
+}
+
+export default Footer;
