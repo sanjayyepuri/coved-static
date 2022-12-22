11d3b001c8b64ca12eae4d27dc7474a81dd112f2
eraserbird <birddrawer@gmail.com>
2022-12-22 11:02:22 -0500

    donate page added

---
+++ src/content/Donate/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components";
+import { FONTS, COLORS } from "../../constants";
+import Layout from "../../components/Layout";
+
+import { Link } from 'gatsby';
+
+const TextThemes = {
+  fontSize: {
+    default: "max(16px,1vw)",
+    lg: "max(24px,1.2vw)",
+  },
+  fontWeight: {
+    default: "400",
+    lg: "700",
+  },
+};
+
+const LinkStyled = styled(Link)`
+  font-family: ${FONTS.font1};
+  font-color: ${COLORS.blue};
+  padding-right: 40px;
+  font-size: ${(props) => TextThemes.fontSize[props.ver]};
+  font-weight: ${(props) => TextThemes.fontWeight[props.ver]};
+  text-decoration: none;
+  &:link {
+    color: ${COLORS.blue};
+  }
+  &:visited {
+    color: ${COLORS.blue};
+  }
+  &:hover {
+    color: ${COLORS.yellow};
+    text-decoration: none;
+  }
+  &:active {
+    color: ${COLORS.yellow};
+    text-decoration: none;
+  }
+`;
+
+const Title = styled.h1`
+  padding-top: 20px;
+  font-size: 3em;
+  font-weight: 400;
+  font-family: ${FONTS.font2};
+  left: calc(50% - 267px/2 + 0.5px)
+  top: 86px
+`;
+
+const Donate = () => {
+  return (
+    <Layout>
+    <Title> Donate </Title>
+    <p>
+    We are a nonprofit that offers free tutoring, so if you want to help us out with our operations, please donate!
+    </p>
+      <LinkStyled to="https://www.paypal.com/donate/?hosted_button_id=JL9NRMP4AU546" ver="default">
+        PayPal
+      </LinkStyled>
+    </Layout>
+  );
+};
+
+export default Donate;
